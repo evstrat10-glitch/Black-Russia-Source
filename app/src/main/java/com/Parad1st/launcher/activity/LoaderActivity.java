15419e7c063fea78955388ae62f27cae85212a31
evstrat10-glitch <evstrat10@gmail.com>
2026-04-26 19:14:43 +0300

    Update LoaderActivity.java

--- app/src/main/java/com/Parad1st/launcher/activity/LoaderActivity.java
+++ app/src/main/java/com/Parad1st/launcher/activity/LoaderActivity.java
@@ -1,69 +1,47 @@
 package com.byparad1st.launcher.activity;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.Environment;
-import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
-import com.byparad1st.game.R;
-import com.byparad1st.launcher.other.Utils;
-
-import com.hzy.libp7zip.P7ZipApi;
+import android.os.Bundle;
+// import com.hzy.libp7zip.P7ZipApi; // Закомментировано — библиотека недоступна
 import com.liulishuo.filedownloader.BaseDownloadTask;
 import com.liulishuo.filedownloader.FileDownloadSampleListener;
 import com.liulishuo.filedownloader.FileDownloader;
-
+import com.byparad1st.launcher.R;
+import com.byparad1st.launcher.utils.Utils;
+import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
 import java.io.File;
 import java.util.Formatter;
 
 public class LoaderActivity extends AppCompatActivity {
-    RoundCornerProgressBar progressbar;
-    File folder;
+
+    private RoundCornerProgressBar progressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_loader);
+        setContentView(R.layout.activity_loader);
+
+        progressbar = findViewById(R.id.progress_bar);
+        TextView textprogress = findViewById(R.id.loading_percent);
+        TextView textloading = findViewById(R.id.loading_text);
+
+        String url = "https://example.com/black-cache.7z"; // Замените на реальный URL
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/black-cache.7z";
+
         FileDownloader.setup(this);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        startDownload();
-    }
-
-    public void startDownload() {
-        folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-        String url = "www.luxrussia.online/black-cache.7z";
-        createDownloadTask(url, folder.getPath()).start();
-    }
-
-    private BaseDownloadTask createDownloadTask(String url, String path) {
-        return FileDownloader.getImpl().create(url)
-                .setPath(path, true)
-                .setCallbackProgressTimes(100)
-                .setMinIntervalUpdateSpeed(100)
+        FileDownloader.getImpl().create(url)
+                .setPath(path)
                 .setListener(new FileDownloadSampleListener() {
-
-                    @Override
-                    protected void pending(BaseDownloadTask task, int soFarBytes, int totalBytes) {
-                        super.pending(task, soFarBytes, totalBytes);
-                    }
-
                     @Override
                     protected void progress(BaseDownloadTask task, int soFarBytes, int totalBytes) {
                         super.progress(task, soFarBytes, totalBytes);
-                        long progressPercent = soFarBytes * 100L / totalBytes;
-
-                        RoundCornerProgressBar progressbar = (RoundCornerProgressBar) findViewById(R.id.progress_bar);
-
-                        TextView textprogress = (TextView) findViewById(R.id.loading_percent);
-                        TextView textloading = (TextView) findViewById(R.id.loading_text);
-
-                        textloading.setText("Идёт загрузка игры...");
-                        textprogress.setText(new Formatter().format("%s из %s", new Object[]{Utils.bytesIntoHumanReadable(soFarBytes), Utils.bytesIntoHumanReadable(totalBytes)}).toString());
+                        float progressPercent = (float) soFarBytes / (float) totalBytes * 100;
+                        TextView textprogress = findViewById(R.id.loading_percent);
+                        textprogress.setText(new Formatter().format("%s из %s", Utils.bytesIntoHumanReadable(soFarBytes), Utils.bytesIntoHumanReadable(totalBytes)).toString());
                         progressbar.setProgress((int) progressPercent);
                     }
 
@@ -72,7 +50,6 @@ public class LoaderActivity extends AppCompatActivity {
                         super.error(task, e);
                         Toast.makeText(getApplicationContext(), "Произошла ошибка начните заново установку", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(LoaderActivity.this, MainActivity.class));
-
                     }
 
                     @Override
@@ -88,28 +65,31 @@ public class LoaderActivity extends AppCompatActivity {
                     @Override
                     protected void completed(BaseDownloadTask task) {
                         super.completed(task);
-                        TextView textprogress = (TextView) findViewById(R.id.loading_percent);
-                        TextView textloading = (TextView) findViewById(R.id.loading_text);
+                        TextView textprogress = findViewById(R.id.loading_percent);
+                        TextView textloading = findViewById(R.id.loading_text);
 
                         textloading.setText("Идёт распаковка файлов игры...");
                         textprogress.setText("2/2");
-                        UnZipCache();
+                        // UnZipCache(); // Закомментировано — библиотека P7ZipApi недоступна
+                        afterDownload();
                     }
 
                     @Override
                     protected void warn(BaseDownloadTask task) {
                         super.warn(task);
                     }
-                });
+                }).start();
     }
 
+    /*
+    // Метод UnZipCache закомментирован, так как библиотека P7ZipApi недоступна
     public void UnZipCache(){
         String mInputFilePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/black-cache.7z";
         String mOutputPath = Environment.getExternalStorageDirectory().toString();
         new Thread() {
             @Override
             public void run() {
-                P7ZipApi.executeCommand(String.format("7z x '%s' '-o%s' -aoa", mInputFilePath, mOutputPath));
+                // P7ZipApi.executeCommand(String.format("7z x '%s' '-o%s' -aoa", mInputFilePath, mOutputPath));
                 Utils.delete(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/black-cache.7z"));
                 Utils.delete(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/black-cache.7z.temp"));
                 runOnUiThread(() -> {
@@ -118,9 +98,10 @@ public class LoaderActivity extends AppCompatActivity {
             }
         }.start();
     }
-    
+    */
+
     public void afterDownload(){
-    	Toast.makeText(this, "Игра успешно установлена!", Toast.LENGTH_SHORT).show();
-         startActivity(new Intent(this, com.byparad1st.launcher.activity.MainActivity.class));
+        Toast.makeText(this, "Игра успешно установлена!", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
