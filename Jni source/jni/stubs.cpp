73f2a114f8e2c99b77837485c397bf4306d5a7db
evstrat10-glitch <evstrat10@gmail.com>
2026-04-26 03:04:23 +0300

    Update stubs.cpp

--- Jni source/jni/stubs.cpp
+++ Jni source/jni/stubs.cpp
@@ -1,22 +1,22 @@
 #include "stubs.h"
 
-// Заглушки для недостающих RPC-функций
+// Точные имена из ошибок
 void RPC_ScrInterpolateCamera() {}
-void RSC_AddGangZone() {}
+void RPC_ScrAddGangZone() {}
 void RPC_ScrRemoveGangZone() {}
-void RSC_ScrFlashGangZone() {}
+void RPC_ScrFlashGangZone() {}
 void RPC_ScrStopFlashGangZone() {}
-void RSC_ScrCreateObject() {}
+void RPC_ScrCreateObject() {}
 void RPC_ScrDestroyObject() {}
-void RSC_ScrSetObjectPos() {}
+void RPC_ScrSetObjectPos() {}
 void RPC_ScrPlaySound() {}
-void RSC_ScrSetPlayerWantedLevel() {}
+void RPC_ScrSetPlayerWantedLevel() {}
 void RPC_ScrRemoveComponent() {}
-void RSC_ScrSetObjectRotation() {}
+void RPC_ScrSetObjectRotation() {}
 void RPC_ClickTextDraw() {}
 
-// Заглушки для RakNet
-namespace RAKlet {
+// RakNet — правильное имя
+namespace RAkNet {
     void BitStream::Write1() {}
     void BitStream::Write0() {}
 }
